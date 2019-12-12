f4c2df628f5088b0495dd62b61ca091fb8d16040
ccs96307 <ccs96307@gmail.com>
2019-12-12 13:47:42 +0800

    test

---
+++ attributes.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Characters Attributes
+"""
+
+class attribute:
+    def __init__(self):
+        self.STR = 100
+        self.DEF = 20
+
+    def skill(self):
+        self.skills = ['猛龍掌']
